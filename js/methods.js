daa3b28057b363e84935fe4f2428d7fd49b79aa3
ulvieyvazov <eyvazovulvi010101@gmail.com>
2023-04-20 19:41:20 +0400

    I added array method ozum y6azmisam metodlari for versionunu

---
+++ js/methods.js
@@ -0,0 +1,98 @@
+//task1.1:foreach methodunu ozun yaz
+
+//foreachin mentiqi oduki arrayda gostermir ve elementlerini baska array a push ede bilmir
+
+// const arrayItems = ['salam', 'necesen', 'yaxshiyam'];
+
+// for(let i = 0;i<arrayItems.length;i++){
+//      console.log(i,arrayItems[i])
+// }
+
+
+//TASK1.2: Map methodunu ozun yaz
+
+//map baska bos arrlar yaradib  onlara push ede bilir
+
+// arr = [1,2,34,45,3]
+
+// secondarr = []
+// for (let i=0;i<arr.length;i++){
+//     secondarr.push(arr[i]*2)
+// }
+// console.log(secondarr)
+
+
+//Task1.3:Filter methodunu ozun yaz
+
+// arr = [1,2,6,8,10,20,13,44,55,46]
+// for(let i = 0;i<arr.length;i++){
+//      if(arr[i]>10){
+//           console.log(arr[i])
+//      }
+// }
+
+//Task1.4:find metodunu ozun yaz
+
+//filterden ferqli olaraq bir dene tapan kimi dayanir
+
+// arr = [1,2,6,8,10,20,13,44,55,46]
+// for(let i = 0;i<arr.length;i++){
+//      if(arr[i]%2==0){
+//           console.log(arr[i])
+//           break;
+//      }
+// }
+
+//Task1.5:every methodunu özün yaz.
+
+// arr = [1,2,6,8,10,20,13,44,55,46]
+// for(let i = 0;i<arr.length;i++){
+//      if(arr[i]%2==0){
+//           console.log(true)
+//           break;
+
+//      }
+// }
+
+//Task1.6:some methodunun ozun yaz
+
+// arr = [1,2,6,8,10,20,13,44,55,46]
+// for(let i = 0;i<arr.length;i++){
+//      if(arr[i]%2==0){
+//           console.log(true)
+//      }
+//      else{
+//           console.log(false)
+//      }
+// }
+
+//Task1.7:reduce metodunu ozun yaz+
+
+//------------------------------------------------------------------------------------
+
+//Task2.1: array içərisindəki tək ədədləri tapıb başqa bir arraydə göstərin.
+
+// let numbers = [1,2,3,4,5,6,7,8,9]
+// otherarr = []
+// for(let i = 0;i<numbers.length;i++){
+//      if(numbers[i]%2==1){
+//           otherarr.push(numbers[i])
+//      }
+//      }
+// console.log(otherarr)
+
+//Task2.2 and 2.3:alınan yeni arrayin elementlərinin kvadratını alıb başqa bir arraydə göstərin.
+
+// let otherarr = [1, 3, 5, 7, 9]
+// newarr = []
+// for (let i = 0;i<otherarr.length;i++){
+//      if(newarr.push(otherarr[i]**2)>10){
+//           console.log(newarr)
+//      }
+// }
+// console.log(newarr)
+
+//---------------------------------------------------------------------------------------------------------
+
+//TASK3
+
